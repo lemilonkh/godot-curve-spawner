e32b14c54820c50598e4273c8539162ac09a1397
Milan Gruner <milangruner@gmail.com>
2025-09-23 11:00:53 +0200

    Update RotationModifier to new argument list for apply method to fix parse error

--- modifiers/rotation_modifier.gd
+++ modifiers/rotation_modifier.gd
@@ -1,6 +1,6 @@
 @tool
 class_name RotationModifier extends SpawnModifier
 
-func apply(object: Node3D, point: Vector3, up: Vector3, index: int = 0) -> void:
+func apply(object: Node3D, point: Vector3, up: Vector3, forward: Vector3, index: int, curve_progress: float) -> void:
 	var global_up := point + up
 	object.look_at(global_up)
